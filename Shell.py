aeb3b700db3e1868b4f948573ee52104297934ac
Loricsi <ciminata08@gmail.com>
2020-12-20 17:34:17 +0100

    Update
    An update of this shell

--- Shell.py
+++ Shell.py
@@ -2,21 +2,24 @@ import os
 from colorama import Fore, Back, Style
 
 
+
 def clear():
     os.system('cls')
 
 clear()
 
+
+
 def start():
     clear()
-    print(Fore.BLUE + "███████╗██╗  ██╗███████╗██╗     ██╗         ███╗   ██╗ █████╗ ███╗   ███╗███████╗")
-    print(Fore.BLUE + "██╔════╝██║  ██║██╔════╝██║     ██║         ████╗  ██║██╔══██╗████╗ ████║██╔════╝")
-    print(Fore.BLUE + "███████╗███████║█████╗  ██║     ██║         ██╔██╗ ██║███████║██╔████╔██║█████╗  ")
-    print(Fore.BLUE + "╚════██║██╔══██║██╔══╝  ██║     ██║         ██║╚██╗██║██╔══██║██║╚██╔╝██║██╔══╝  ")
-    print(Fore.BLUE + "███████║██║  ██║███████╗███████╗███████╗    ██║ ╚████║██║  ██║██║ ╚═╝ ██║███████╗")
-    print(Fore.BLUE + "╚══════╝╚═╝  ╚═╝╚══════╝╚══════╝╚══════╝    ╚═╝  ╚═══╝╚═╝  ╚═╝╚═╝     ╚═╝╚══════╝")
+    print(Fore.BLUE + '''    ███████╗██╗  ██╗███████╗██╗     ██╗         ███╗   ██╗ █████╗ ███╗   ███╗███████╗
+    ██╔════╝██║  ██║██╔════╝██║     ██║         ████╗  ██║██╔══██╗████╗ ████║██╔════╝
+    ███████╗███████║█████╗  ██║     ██║         ██╔██╗ ██║███████║██╔████╔██║█████╗  
+    ╚════██║██╔══██║██╔══╝  ██║     ██║         ██║╚██╗██║██╔══██║██║╚██╔╝██║██╔══╝  
+    ███████║██║  ██║███████╗███████╗███████╗    ██║ ╚████║██║  ██║██║ ╚═╝ ██║███████╗
+    ╚══════╝╚═╝  ╚═╝╚══════╝╚══════╝╚══════╝    ╚═╝  ╚═══╝╚═╝  ╚═╝╚═╝     ╚═╝╚══════╝''')
+    #YOU CAN FIND THIS TEXT ON https://fsymbols.com/generators/carty/ OR ON http://patorjk.com/software/taag/#p=display&f=ANSI%20Shadow&t=
 prefix = "</>"
-
 
 def system_it():
     print("CIAO")
@@ -74,16 +77,71 @@ def system_en():
             clear()
             print(Style.RESET_ALL + Fore.RED + script + ' ' + Style.RESET_ALL + Style.RESET_ALL + "Error: command not found" + Style.RESET_ALL)
             
-        
+    
 def lang():
-    scelta = input("Write 'ita' to go in italian mode, write 'eng' to go in english mode ")
+
     if scelta == "ita":
         system_it()
+        
     elif scelta == "eng":
         system_en()
+        
     else:
         print("This language isn't supported")
         lang()
-start()
-lang()
 
+
+def credits():
+    print("")
+    
+    file_l = open("not_modify/c.txt", "r").read()
+        
+    if file_l == "accept":
+        
+    
+        start()
+        lang()
+        file_l.close()
+        
+    else:
+        if scelta == 'eng':
+
+            print(Fore.MAGENTA + '''This Shell was created by Lorix.
+You can't use this for commercial purposes.
+You can modify him ONLY for personal purposes.
+If you have any questions contact me on Instagram, the link is in the README.md file.''')
+            accepting = input("Do you accept these conditions? If yes write 'accept' else press 'ENTER' ")
+            if accepting == "accept":
+                file_s = open("not_modify/c.txt", "w")
+                file_s.write("accept")
+                file_s.close()
+                start()
+                lang()
+            else:
+                print("Sorry, you can't use this Shell.")
+        elif scelta == 'ita':
+            print(Fore.MAGENTA +'''Questa Shell è stata creata da Lorix.
+Non puoi usarla per scopi commerciali.
+Puoi modificarla SOLO per scopi personali.
+Se hai domande contattami su Instagram, il link si trova nel file README.md.''')
+            accetti = input("Accetti queste condizioni? Se sì scrivi 'accetto' altrimenti premi 'INVIO' ")
+            if accetti == "accetto":
+                file_s = open("not_modify/c.txt", "w")
+                file_s.write("accept")
+                file_s.close()
+                start()
+                lang()
+            else:
+                print("Mi dispiace, non puoi usare questa Shell.")
+        else:
+            print("This language isn't supported")
+
+            
+
+scelta = input(Fore.YELLOW + "Write 'ita' to go in italian mode, write 'eng' to go in english mode ")
+
+credits()
+if scelta == 'ita':
+    input("Qualcosa è andato storto...")
+if scelta == 'eng':
+    input("Something went wrong...")
